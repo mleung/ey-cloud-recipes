084c1a0ac3a4d119ffd3594cb81991f7db95eec7
Michael Leung <blamemike@gmail.com>
2010-03-18 19:27:10 -0400

    Updated custom deployment with xapian bindings

---
+++ cookbooks/xapian_bindings/recipes/default.rb
@@ -0,0 +1,12 @@
+bash "install-xapian " do
+  user "root"
+  code 'echo "dev-libs/xapian ~x86" >> /etc/portage/package.keywords/ec2'
+end
+bash "append-portage" do
+  user "root"
+  code 'echo "dev-libs/xapian-bindings ~x86" >> /etc/portage/package.keywords/ec2'
+end
+execute "emerge-xapian" do
+  command 'emerge --sync'
+  command 'emerge dev-libs/xapian-bindings'
+end
