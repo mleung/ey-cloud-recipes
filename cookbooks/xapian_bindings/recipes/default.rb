3bc85a41c7504c1fb76f282323f6b94a9a9d36bf
Michael Leung <blamemike@gmail.com>
2010-03-18 20:04:03 -0400

    Waxing bs recipes

--- cookbooks/xapian_bindings/recipes/default.rb
+++ cookbooks/xapian_bindings/recipes/default.rb
@@ -1,12 +1,16 @@
-bash "install-xapian " do
-  user "root"
-  code 'echo "dev-libs/xapian ~x86" >> /etc/portage/package.keywords/ec2'
-end
-bash "append-portage" do
-  user "root"
-  code 'echo "dev-libs/xapian-bindings ~x86" >> /etc/portage/package.keywords/ec2'
-end
-execute "emerge-xapian" do
-  command 'emerge --sync'
-  command 'emerge dev-libs/xapian-bindings'
+case node[:instance_role] 
+  when "solo", "app", "app_master"
+    bash "install-xapian " do
+      user "root"
+      code 'echo "dev-libs/xapian ~x86" >> /etc/portage/package.keywords/ec2'
+    end
+    bash "append-portage" do
+      user "root"
+      code 'echo "dev-libs/xapian-bindings ~x86" >> /etc/portage/package.keywords/ec2'
+    end
+    execute "emerge-xapian" do
+      command 'emerge --sync'
+      command 'emerge dev-libs/xapian-bindings'
+    end
+  end
 end
