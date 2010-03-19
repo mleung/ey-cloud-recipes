ad10ed813b6cbe8536658b91c2119434915b5d8d
Michael Leung <blamemike@gmail.com>
2010-03-19 11:54:13 -0400

    trying to loop through all nodes

--- cookbooks/xapian_bindings/recipes/default.rb
+++ cookbooks/xapian_bindings/recipes/default.rb
@@ -1,14 +1,22 @@
 require 'pp'
 
-bash "install-xapian " do
-  user "root"
-  code 'echo "dev-libs/xapian ~x86" >> /etc/portage/package.keywords/ec2'
-end
-bash "append-portage" do
-  user "root"
-  code 'echo "dev-libs/xapian-bindings ~x86" >> /etc/portage/package.keywords/ec2'
-end
-execute "emerge-xapian" do
-  command 'emerge --sync'
-  command 'emerge dev-libs/xapian-bindings'
+node[:applications].each do |app_name,data|
+  
+  
+  case node[:instance_role] 
+    when "solo", "app", "app_master"
+      bash "install-xapian " do
+        user "root"
+        code 'echo "dev-libs/xapian ~x86" >> /etc/portage/package.keywords/ec2'
+      end
+      bash "append-portage" do
+        user "root"
+        code 'echo "dev-libs/xapian-bindings ~x86" >> /etc/portage/package.keywords/ec2'
+      end
+      execute "emerge-xapian" do
+        command 'emerge --sync'
+        command 'emerge dev-libs/xapian-bindings'
+      end
+  end
+
 end
