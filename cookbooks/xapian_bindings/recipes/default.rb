bbffc332345a3de2f79e595d14da6cc5a3c6d468
Michael Leung <blamemike@gmail.com>
2010-03-19 11:43:05 -0400

    Require pp line

--- cookbooks/xapian_bindings/recipes/default.rb
+++ cookbooks/xapian_bindings/recipes/default.rb
@@ -1,17 +1,14 @@
 require 'pp'
 
-case node[:instance_role] 
-  when "solo", "app", "app_master"
-    bash "install-xapian " do
-      user "root"
-      code 'echo "dev-libs/xapian ~x86" >> /etc/portage/package.keywords/ec2'
-    end
-    bash "append-portage" do
-      user "root"
-      code 'echo "dev-libs/xapian-bindings ~x86" >> /etc/portage/package.keywords/ec2'
-    end
-    execute "emerge-xapian" do
-      command 'emerge --sync'
-      command 'emerge dev-libs/xapian-bindings'
-    end
+bash "install-xapian " do
+  user "root"
+  code 'echo "dev-libs/xapian ~x86" >> /etc/portage/package.keywords/ec2'
+end
+bash "append-portage" do
+  user "root"
+  code 'echo "dev-libs/xapian-bindings ~x86" >> /etc/portage/package.keywords/ec2'
+end
+execute "emerge-xapian" do
+  command 'emerge --sync'
+  command 'emerge dev-libs/xapian-bindings'
 end
